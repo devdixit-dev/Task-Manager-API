605d0a8b89d474638d2e6f247f707f59b3f744b9
Dev Dixit <devdixitsocial@gmail.com>
2025-11-12 12:37:16 +0530

    test: remove obsolete authentication test file

--- src/tests/auth/auth.test.ts
+++
@@ -1,8 +0,0 @@
-import request from 'supertest';
-import app from '../../index.js';
-
-test("POST /api/auth/check check the route", async () => {
-  const res = await request(app).get('/check');
-  expect(res.status).toBe(200);
-  expect(res.body).toEqual({ success: true })
-});
